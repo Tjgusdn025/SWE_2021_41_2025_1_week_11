ad3b21ca14f77262d6f68d8c89be20fb6a394c3c
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:36:41 +0900

    path_to_file_list implemented

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    # Will this be working?
     lines = open(path, 'r').read().split('\n')
     return lines
 
@@ -24,25 +23,13 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     template_mid = '\",\"German\":\"'
     template_end = '\"}'
 
-<<<<<<< HEAD
-    # Can this be working?
-    processed_file_list = []
-    for english_file, german_file in zip(english_file_list, german_file_list):
-        english_file = process_file(english_file)
-        english_file = process_file(german_file)
-
-        processed_file_list.append(template_end + english_file + template_mid + german_file + template_start)
-    return processed_file_list
-
-=======
     processed_file_list = []
     for english_file, german_file in zip(english_file_list, german_file_list):
         english_file = process_file(english_file)
         german_file = process_file(german_file)
 
-        processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
+    processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
     return processed_file_list
->>>>>>> 3909f1b (train_file_list_to_json implemented)
 
 def write_file_list(file_list: List[str], path: str) -> None:
     """Writes a list of strings to a file, each string on a new line"""
